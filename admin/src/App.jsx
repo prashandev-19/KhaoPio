a27e9a400c71c8b19c360aa5addd4ee01c6bdee1
Dev <prashandev2000@gmail.com>
2025-07-03 01:23:19 +0530

    Working on admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Sidebar from './components/Sidebar/Sidebar.jsx'
+import { Routes ,Route } from 'react-router-dom'
+import Add from './pages/Add/Add.jsx'
+import List from './pages/List/List.jsx'
+import Orders from './pages/Orders/Orders.jsx'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+         <Routes>
+          <Route path="/add" element = {<Add/>} />
+          <Route path="/list" element = {<List/>} />
+          <Route path = "/orders" element = {<Orders />} />
+         </Routes>
+      </div>
     </div>
   )
 }
